a09d31715bb44d0d9ebe69b2929693219beafa5a
Beatriz Seraphim <beatrizoseraphim@gmail.com>
2023-03-30 13:53:34 -0300

    Produtos Aves

--- src/javapetfood/model/ProdutoAves.java
+++ src/javapetfood/model/ProdutoAves.java
@@ -1,5 +1,24 @@
 package javapetfood.model;
 
-public class ProdutoAves {
+import java.time.LocalDate;
+
+public class ProdutoAves extends Produto {
+	
+	private int especies;
+
+	public ProdutoAves(int tipo, String nome, int codigo, float preco, LocalDate dataValidade, int quantidadeEstoque,
+			int especies) {
+		super(tipo, nome, codigo, preco, dataValidade, quantidadeEstoque);
+		this.especies = especies;
+	}
+
+	public int getEspecies() {
+		return especies;
+	}
+
+	public void setEspecies(int especies) {
+		this.especies = especies;
+	}
+	
 
 }
